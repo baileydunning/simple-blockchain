153c540812f92969b158099ea57922577dc0e64c
Bailey Dunning <bwdunning@gmail.com>
2021-03-28 20:25:17 -0600

    add mining method

--- src/Block.js
+++ src/Block.js
@@ -5,10 +5,21 @@ class Block {
     this.timestamp = timestamp
     this.data = data
     this.previousHash = previousHash
+    this.hash = this.calculateHash()
+    this.nonce = 0
   }
 
   calculateHash() {
     return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
+  }
+
+  mineBlock(difficulty) {
+    while(this.previousHash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+      this.nonce++
+      this.hash = this.calculateHash()
+    }
+
+    console.log('Block mined: ', this.hash)
   }
 }
 
