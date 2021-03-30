b4f1e203edf5aa56e5e0918c70ed33f2e636c67b
Bailey Dunning <bwdunning@gmail.com>
2021-03-29 19:13:54 -0600

    add console logs to index

--- index.js
+++ index.js
@@ -3,13 +3,8 @@ const Blockchain = require('./src/Blockchain')
 
 let baileyCoin = new Blockchain()
 
+console.log('Mining block 1...')
 baileyCoin.addBlock(new Block(1, '3/28/2021', { amount: 2 }))
+
+console.log('Mining block 2...')
 baileyCoin.addBlock(new Block(2, '3/28/2021', { amount: 10 }))
-
-console.log('Is blockchain valid?', baileyCoin.isChainValid())
-
-baileyCoin.chain[1].data = {amount: 100}
-
-console.log('Is blockchain valid?', baileyCoin.isChainValid())
-
-// console.log(JSON.stringify(baileyCoin))
